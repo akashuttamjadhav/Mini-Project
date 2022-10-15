73864b73ef61f0c3f957d90c60210670ed564f85
krushna patil <krushnapatil34@gmail.com>
2022-10-15 21:50:42 +0530

    login page added

---
+++ GadgetStore/src/com/Login/LoginPage.java
@@ -0,0 +1,26 @@
+package com.Login;
+import java.util.Scanner;
+public class LoginPage {
+	
+
+	
+
+	    public void login() {
+
+	        try (Scanner scanner = new Scanner(System.in)) {
+	            System.out.print(" Enter user name => ");
+	            String userName = scanner.nextLine();
+
+	            System.out.print(" Enter password => ");
+	            String password = scanner.nextLine();
+
+	            if ("ramesh".equals(userName) && "password".equals(password)) {
+	                System.out.println(" User successfully logged-in.. ");
+	            } else {
+	                System.out.println(" In valid userName of password ");
+	            }
+	        }
+	    }
+	}
+
+
